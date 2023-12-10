5602e7018cba4e20b7edf52c123548077e2f20bb
Carlos Loaeza <carlos.loaeza@icloud.com>
2023-12-09 22:53:33 -0600

    Update Controller

--- product/src/main/java/com/ibm/product/product/controller/ProductController.java
+++ product/src/main/java/com/ibm/product/product/controller/ProductController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestController
 @RequestMapping("/product")
@@ -28,12 +30,14 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @PostMapping("/create")    
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)    
     public Product create(@RequestBody Product product){
         return productService.save(product);
     }
 
     @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT) 
     public void delete(@PathVariable Long id){
         productService.deleteById(id);
     }
