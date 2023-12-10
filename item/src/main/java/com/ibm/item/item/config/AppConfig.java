ed9e0a70018a044030f5dcd7774e5549150813b2
Carlos Loaeza <carlos.loaeza@icloud.com>
2023-12-10 17:21:29 -0600

    Circuit breaker

--- item/src/main/java/com/ibm/item/item/config/AppConfig.java
+++ item/src/main/java/com/ibm/item/item/config/AppConfig.java
@@ -1,9 +1,17 @@
 package com.ibm.item.item.config;
 
+import java.time.Duration;
+
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 
 @Configuration
 public class AppConfig {
@@ -12,6 +20,22 @@ public class AppConfig {
     @LoadBalanced
     public RestTemplate registerRestTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer(){
+        return factory -> factory.configureDefault(id -> {
+            return new Resilience4JConfigBuilder(id)
+            .circuitBreakerConfig(CircuitBreakerConfig.custom().slidingWindowSize(10)
+            .failureRateThreshold(50)
+            .waitDurationInOpenState(Duration.ofSeconds(10))
+            .permittedNumberOfCallsInHalfOpenState(5)
+            .slowCallRateThreshold(50)
+            .slowCallDurationThreshold(Duration.ofSeconds(2L))
+            .build())
+            .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(3L)).build())
+            .build();
+        });
     }
 
 }
--- item/src/main/java/com/ibm/item/item/controller/ItemController.java
+++ item/src/main/java/com/ibm/item/item/controller/ItemController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ibm.item.item.model.Product;
 import com.ibm.item.item.service.ItemService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+
 @RestController
 @RequestMapping("/item")
 public class ItemController {
@@ -25,26 +30,59 @@ public class ItemController {
     @Qualifier("serviceRest")
     private ItemService itemService;
 
+    @Autowired
+    private CircuitBreakerFactory circuitBreakerFactory;
+
     @GetMapping("/list-all")
-    public List<Product> list(){
+    public List<Product> list() {
         return itemService.findAll();
     }
 
+    @CircuitBreaker(name = "items", fallbackMethod = "alternativeMethod")
     @GetMapping("/find/{id}")
-    public Product findById(@PathVariable Long id){
+    public Product findById(@PathVariable Long id) {
         return itemService.findById(id);
+    }
+
+    @CircuitBreaker(name = "items", fallbackMethod = "alternativeMethod2")
+    @GetMapping("/detail/{id}")
+    public ResponseEntity<Product> detail(@PathVariable Long id){
+        Product product = itemService.findById(id);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @GetMapping("/info/{id}")
+    public Product info(@PathVariable Long id){
+        return circuitBreakerFactory.create("items").run(() -> itemService.findById(id),
+        e -> alternativeMethod(e));
     }
 
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
-    public Product create(@RequestBody Product product){
+    public Product create(@RequestBody Product product) {
         return itemService.save(product);
     }
 
     @DeleteMapping("/delete/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id){
+    public void delete(@PathVariable Long id) {
         itemService.delete(id);
+    }
+
+    public Product alternativeMethod(Throwable e) {
+        Product product = new Product();
+        product.setId(1L);
+        product.setName("Default product");
+        product.setPrice(0.0);
+        return product;
+    }
+
+    public ResponseEntity<Product> alternativeMethod2(Throwable e) {
+        Product product = new Product();
+        product.setId(1L);
+        product.setName("Default product");
+        product.setPrice(0.0);
+        return new ResponseEntity<>(product, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
